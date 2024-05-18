4a52e46e990b8b7b7f51cf9f3974371de4e14789
ekremkeser <ekremkeser23@gmail.com>
2024-05-18 10:33:24 +0300

    g

--- src/components/Anasayfa.jsx
+++ src/components/Anasayfa.jsx
@@ -1,15 +1,15 @@
 
 import { NavLink, Link } from 'react-router-dom/cjs/react-router-dom.min';
-import Header from './Header';
-import App from '../App';
+
 
 export default function Anasayfa() {
 
   return (
-    < >
+    <>
     <header>
     <h1>Teknolojik Yemekler</h1>
     </header>
+    <div className='mainpage-back'>
     <div className='main-page-body'>
         <p>
           fırsatı kaçırma
@@ -24,7 +24,7 @@ export default function Anasayfa() {
             <Link to="/siparis-ver" className="main-page-button">ACIKTIM</Link>
         </nav>
     </div>
-    <footer className='mainpage-footer'>
+    <div className='mainpage-footer'>
     <nav>
             <Link to="/siparis-ver" className="main-page-kore">YENİ!! Kore</Link>
     </nav>
@@ -43,7 +43,8 @@ export default function Anasayfa() {
     <nav>
             <Link to="/siparis-ver" className="main-page-gazli">Gazlı İçecek</Link>
     </nav>
-    </footer>
+    </div>
+    </div>
     </>
     
     
